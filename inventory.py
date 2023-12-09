0c92cb5cd4b45bc4cecdaaeb821de1ae01384263
DeziBacha <111678991+DeziBacha@users.noreply.github.com>
2023-12-09 21:53:15 +0400

    Adding file

--- inventory.py
+++ inventory.py
@@ -1,6 +1,6 @@
-
+import time
 global products
-products = [[]]
+products = [["1","chips",15,"solid","20g",12]]
 
 
 
@@ -32,24 +32,60 @@ def start():
         print("Thank you for using Inventory Manager")
         
         
+        
+
+        
+        
 def add():
     #code to add item
+    global item_id 
+    item_id = int(input("Enter item ID: "))
+    global item_name 
+    item_name = input("Enter item name: ")
+    global item_quantity 
+    item_quantity = int(input("Enter item quantity: "))
+    global item_type 
+    item_type = input("Enter item type: ")
+    global item_price 
+    item_price = float(input("Enter item price: "))
     
+    products.append([item_id,item_name,item_quantity,item_type,item_price])
     
+    print("Item added successfully")
+    time.sleep(1)
+    start()
     
 
 def remove():
     #code to remove item
+    print("Enter item name to remove: ")
+    remove = input("")
+    def search_1 (arr, target):
+        for i in range(len(arr)):
+            for j in range(len(arr[i])):
+                if (arr[i][j] == target):
+                    del(products[i])
+                    print(f"Item {target} removed successfully")
+                    start()
+        print("Item not found")
+    
+    # Driver code
+    arr = products
+    target = remove
+    
+    search_1(arr, target)
+    
+    
 
-def update():
-    #code to update item
 
-def search():
-    #code to search item
 
 def print_items():
-    #code to print all items    
-        
+    #code to print all items
+    for i in range(len(products)):
+        for j in range(len(products[i])):
+            print(products[i][j],end=" | ")
+            time.sleep(1)
+            start()        
         
 
 
