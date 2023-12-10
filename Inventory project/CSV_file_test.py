0216334f464adfc9fc0712b91d13ad3a195934c2
DeziBacha <111678991+DeziBacha@users.noreply.github.com>
2023-12-10 15:41:22 +0400

    change

--- Inventory project/CSV_file_test.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open('aapl.csv', 'r') as csv_file:
-    reader = csv.reader(csv_file)
-
-    for row in reader:
-        print(row)
--- Inventory project/Pandas_test.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('aapl.csv')
-
-print(df.to_string())
