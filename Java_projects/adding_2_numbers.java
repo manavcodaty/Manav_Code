b20c4fd5f5abd44b922f3448a830a46226e35fcc
manavcodaty <manav.codaty@icloud.com>
2024-01-04 18:57:03 +0400

    voting system + java programs

--- Java_projects/adding_2_numbers.java
+++ Java_projects/adding_2_numbers.java
@@ -4,12 +4,13 @@ public class adding_2_numbers {
     public static void main(String[] args) {
         int num1 = 0;
         int num2 = 0;
-        Scanner myobj = new Scanner(System.in);
+        Scanner myObj = new Scanner(System.in);
         System.out.println("Enter first number: ");
-        num1 = myobj.nextInt();
+        num1 = myObj.nextInt();
         System.out.println("Enter second number: ");
-        num2 = myobj.nextInt();
+        num2 = myObj.nextInt();
         System.out.println("Sum of " + num1 + " and " + num2 + " is " + (num1 + num2));
+        myObj.close();
 
         
 
